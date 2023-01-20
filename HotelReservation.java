ea8b5e46e022d0f13881c359ea9e0ebc46181d96
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-20 13:15:48 +0530

    Added -Ability to Find Cheapest And Best Rated Hotel

--- HotelReservation.java
+++ HotelReservation.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 public class HotelReservation {
-	  List<Hotel> hotelDetails = new ArrayList<>();
+	 List<Hotel> hotelDetails = new ArrayList<>();
 	    public void addHotelDetails(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForRewarder, int weekendRateForRewarder, int hotelRatings) {
 	        Hotel hotel= new Hotel(hotelName, weekdayRateForRegular, weekendRateForRegular, weekdayRateForRewarder, weekendRateForRewarder, hotelRatings);
 	        hotelDetails.add(hotel);
@@ -30,15 +30,47 @@ public class HotelReservation {
 	        System.out.println("The cheapest Hotel Rate is " +cheapestHotel);
 	    }
 
-	    public void viewHotels() {
-	        getHotelDetails();
+	    public void getCheapestHotelForGivenDate() {
+	        String hotel =null;
+	        int totalLakeWoodCost = 0, totalBridgeWoodCost = 0, totalRidgeWoodCost= 0;
+	        int LakeWood_Rate = 3, BridgeWood_Rate = 4, RidgeWood_Rate = 5;
+	        if (Objects.equals(hotel, "LakeWood")) {
+	            int weekday_rate = 110;
+	            int weekend_rate = 90;
+	            totalLakeWoodCost = (totalLakeWoodCost + weekday_rate + weekend_rate);
+	        }
+	        if (Objects.equals(hotel, "BridgeWood")) {
+	            int weekday_rate = 150;
+	            int weekend_rate = 50;
+	            totalBridgeWoodCost = (totalBridgeWoodCost + weekday_rate + weekend_rate);
+	        }
+	        if (Objects.equals(hotel, "RidgeWood")) {
+	            int weekday_rate = 220;
+	            int weekend_rate = 150;
+	            totalRidgeWoodCost = (totalRidgeWoodCost + weekday_rate + weekend_rate);
+	        }
+	        if ((totalBridgeWoodCost <= totalLakeWoodCost && totalBridgeWoodCost < totalRidgeWoodCost) && (totalLakeWoodCost < totalBridgeWoodCost && totalBridgeWoodCost < totalRidgeWoodCost)) {
+	            System.out.println("BridgeWood");
+	            System.out.println("Rating:" + BridgeWood_Rate);
+	            System.out.println("Total Rates: $" + totalBridgeWoodCost );
+	        }
 	    }
-	    public static void main(String[] args) {
+
+	    public void viewHotels() {
+	        getCheapestHotelForGivenDate();
+	    }
+	    public static void main(String[] args) throws ParseException {
 	        System.out.println("Welcome to Hotel Reservation Program");
 	        HotelReservation hotelReservation = new HotelReservation();
 	        hotelReservation.addHotelDetails("Lakewood", 110, 80, 90, 80, 3);
 	        hotelReservation.addHotelDetails("Bridgewood", 160, 110, 60, 50, 4);
 	        hotelReservation.addHotelDetails("Ridgewood", 220, 110, 150, 40, 5);
+	        String startDate ="10-09-2020";
+	        String endingDate ="11-09-2020";
+	        java.util.Date sdate =new SimpleDateFormat("dd-MM-yyyy").parse(startDate);
+	        java.util.Date edate =new SimpleDateFormat("dd-MM-yyyy").parse(endingDate);
+	        System.out.println(startDate +"\t"+ sdate);
+	        System.out.println(endingDate +"\t"+ edate);
 	        hotelReservation.viewHotels();
 	    }
 }

40d1d6afdc49079003f89ccf76addd0e9882d6f8
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-20 12:49:05 +0530

    Added -Hotel Reservation

---
+++ HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day26.UC_Day26_Hotel_Reservation;
+
+public class HotelReservation {
+	
+	 public static void main(String[] args) {
+	System.out.println("Welcome to Hotel Reservation Program");
+
+}
+}
